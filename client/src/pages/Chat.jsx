5836f266b0d3d38f5404e4ff9879ff33455d1779
chan young <white1614@naver.com>
2024-02-06 15:14:36 +0900

    chat 정보 가져오기

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,5 +1,28 @@
+import axios from "axios"
+import { useQuery } from "react-query"
+import { useRecoilValue } from "recoil"
+import { userData } from "../recoil/Auth"
 
 const Chat = () => {
+
+  const userId = useRecoilValue(userData)[0]?._id
+
+  const findChat = async() => {
+    const response = await axios.get(`http://localhost:4040/api/chats/${userId}`);
+    return response.data;
+  }
+
+  const { data, error, isLodaing} = useQuery({
+    queryKey: ['findChat'],
+    queryFn: findChat
+  })
+
+  console.log(data)
+
+  if(isLodaing) return <p>Loding...</p>
+
+  if(error) return <p>Something is wrong..</p>
+
   return (
     <div>Chat</div>
   )
